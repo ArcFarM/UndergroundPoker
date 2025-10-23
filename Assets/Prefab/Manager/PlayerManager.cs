36b974f5bd2c22cf2c3fb6135624e76c6a5f5e96
ArcFarM <hinsung99@gmail.com>
2025-10-23 21:09:59 +0900

    Update PlayerManager.cs

--- Assets/Prefab/Manager/PlayerManager.cs
+++ Assets/Prefab/Manager/PlayerManager.cs
@@ -1,8 +1,24 @@
-﻿using UnityEngine;
+﻿using UnderGroundPoker.Prefab.Card;
+using UnityEngine;
 
 namespace UnderGroundPoker.Prefab.Manager {
     public class PlayerManager : MonoBehaviour {
-
+        #region player variables
+        //플레이어 목숨(칩)
+        int playerLife;
+        [SerializeField] int initialLife = 10;
+        [SerializeField] int maxLife = 10;
+        [SerializeField] int currentBet = 0;
+        //플레이어 목숨 프로퍼티
+        public int PlayerLife {
+            get { return playerLife; }
+            set {
+                playerLife = Mathf.Clamp(value, 0, maxLife);
+            }
+        }
+        //player hand
+        PlayerHand playerHand;
+        #endregion
     }
 
 }
